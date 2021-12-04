a1cf171529b3246fa279359fcc0c09cac92a44f8
Madrass <Madrass@LAPTOP-GE9TSN73>
2021-12-04 19:49:22 +0100

    basic concepts

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import datetime
+
+from PIL import ImageGrab #pip install Pillow
+import numpy as np # gets imported with cv2
 import cv2
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from win32api import GetSystemMetrics # pip install pywin32
 
+interval = 60
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+file_name = f'{time_stamp}.mp4'
+print(time_stamp)
 
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#webcam part
+web_cam = cv2.VideoCapture(0) # 0 gets the first camera
+#webcampart end
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    img = ImageGrab.grab(bbox=(0,0, width, height))
+    img_np = np.array(img)
+    img_final = cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+
+    #wecam part
+
+    _, frame = web_cam.read()
+    frameheight, frameWidth, _ = frame.shape
+    # cv2.imshow('web cam', frame)
+    img_final[0:frameheight, 0:frameWidth, :] = frame [0: frameheight, 0: frameWidth, : ]
+
+    #webcam part end
+    cv2.imshow('secret name', img_final)
+    captured_video.write(img_final)
+    if cv2.waitKey(interval) == ord('q'):
+        break
