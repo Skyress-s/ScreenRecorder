d10a1159485de0f9aee013effdf67d8233e8b621
Skyress-s <mathmoe64@gmail.com>
2021-12-05 01:48:06 +0100

    work on getting second screen to work

--- main.py
+++ main.py
@@ -5,32 +5,47 @@ import numpy as np # gets imported with cv2
 import cv2
 from win32api import GetSystemMetrics # pip install pywin32
 
-interval = 60
 
-width = GetSystemMetrics(0)
-height = GetSystemMetrics(1)
+from desktopmagic.screengrab_win32 import (
+getDisplayRects, saveScreenToBmp, saveRectToBmp, getScreenAsImage,
+getRectAsImage, getDisplaysAsImages)
+
+
+interval = 80
+
+# width = GetSystemMetrics(0)
+# height = GetSystemMetrics(1)
 time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 file_name = f'{time_stamp}.mp4'
 print(time_stamp)
 
+monitor = 1 # 0 is the first monitor, 1 is the second monitor
+
+
+rects = getDisplayRects()
+print(rects)
+
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
-
-#webcam part
-web_cam = cv2.VideoCapture(0) # 0 gets the first camera
-#webcampart end
-
+# here we have to get the correct screen size or else we get an error, we there det the delta og the display rect
+captured_video = cv2.VideoWriter(file_name, fourcc, 40.0, (rects[monitor][2] - rects[monitor][0], rects[monitor][3] - rects[monitor][1]))
 while True:
-    img = ImageGrab.grab(bbox=(0,0, width, height))
+    # grabs the image
+    img = ImageGrab.grab(bbox=(rects[monitor][0], rects[monitor][1], rects[monitor][2], rects[monitor][3]))
+    # img = ImageGrab.grab(bbox=(2564, 360, 4470, 1436))
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
 
+    #img_new = pyautogui.screenshot()
+    #img_new_np = np.array(img_new)
+
+
+
     #wecam part
 
-    _, frame = web_cam.read()
-    frameheight, frameWidth, _ = frame.shape
-    # cv2.imshow('web cam', frame)
-    img_final[0:frameheight, 0:frameWidth, :] = frame [0: frameheight, 0: frameWidth, : ]
+    #_, frame = web_cam.read()
+    #frameheight, frameWidth, _ = frame.shape
+    # # # # cv2.imshow('web cam', frame)
+    #img_final[0:frameheight, 0:frameWidth, :] = frame [0: frameheight, 0: frameWidth, : ]
 
     #webcam part end
     cv2.imshow('secret name', img_final)
