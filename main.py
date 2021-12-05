a795b23a4588f09e12d25d32e8bfddef14bfa7b5
Skyress-s <mathmoe64@gmail.com>
2021-12-05 12:35:03 +0100

    its working!! (mss lib)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
 
 from PIL import ImageGrab #pip install Pillow
+from PIL import Image
 import numpy as np # gets imported with cv2
 import cv2
 from win32api import GetSystemMetrics # pip install pywin32
 import pyautogui
+
+from mss.windows import MSS as mss
 
 
 from desktopmagic.screengrab_win32 import (
@@ -20,25 +23,37 @@ time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 file_name = f'{time_stamp}.mp4'
 print(time_stamp)
 
-monitor = 1 # 0 is the first monitor, 1 is the second monitor
+monitor = 2 # 1 is the first monitor, 2 is the second monitor
 
 
 rects = getDisplayRects()
 print(rects)
 
-print(rects[1][1])
+#testing using mss
+with mss() as sct:
+    mon2 = sct.monitors[monitor]
+
+
+# cv2.waitKey(10000)
+
 
 
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 # here we have to get the correct screen size or else we get an error, we there det the delta og the display rect
-captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (rects[monitor][2] - rects[monitor][0], rects[monitor][3] - rects[monitor][1]))
+# captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (rects[monitor][2] - rects[monitor][0], rects[monitor][3] - rects[monitor][1]))
+captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (mon2["width"], mon2["height"]))
 while True:
     # grabs the image
-    img = ImageGrab.grab(bbox=(rects[monitor][0], rects[monitor][1], rects[monitor][2], rects[monitor][3]))
-    img_np = np.array(img)
-    img_final = cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
+    #img = ImageGrab.grab(bbox=(rects[monitor][0], rects[monitor][1], rects[monitor][2], rects[monitor][3]))
 
+    #img_np = np.array(img)
+    #img_final = cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB)
 
+    img_sct = sct.grab(mon2)
+
+    img = Image.frombytes("RGB", img_sct.size, img_sct.bgra, "raw", "BGRX")
+    img_final = np.array(img)
+    img_final = cv2.cvtColor(img_final,cv2.COLOR_BGR2RGB)
 
 
     #wecam part
